2ac08ac1551074cb2ef0960b31e3e30285b56d8e
a-run-kumar-d <arunkumarpkmn@example.com>
2023-07-26 18:38:19 +0530

    first complete commit

--- src/script.js
+++ src/script.js
@@ -1,16 +1,18 @@
 const body = document.querySelector("body");
 const startButton = document.querySelector("#start");
 const progress =  document.querySelector(".progress");
-let resultBar = document.querySelector("#result");
 const container = document.createElement("div");
 const textArea = document.createElement("textarea");
 const guessButton = document.createElement("button");
 
 let flag = 0;
 let res = "start guessing"
+let randomNumber =  ""
+count = 0
 
 startButton.addEventListener("click", () => {
   if (flag == 0) {
+    randomNumber = Math.floor(Math.random() * (100)) + 1;
     progress.style.display = "block"
     container.classList.add("contain");
     document.body.appendChild(container);
@@ -27,7 +29,23 @@ startButton.addEventListener("click", () => {
   }
 });
 
+function checker(input) {
+  if (input > randomNumber) {
+    res = "try Lesser"
+    count++
+  }
+  else if(input<randomNumber){
+    res = "try More"
+    count++
+  }
+  else{
+    res = `Congrats you won in ${count} tries`
+  }
+}
+
 guessButton.addEventListener("click",()=>{
     let inputValue = textArea.value;
-    console.log(inputValue);
+    textArea.value = ""
+    checker(inputValue)
+    progress.innerHTML = res;
 })
